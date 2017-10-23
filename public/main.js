7ad4f7d00645cbc3ea096ba36cb2410f80ca5636
Robin Thomas <>
2017-10-23 17:41:10 -0500

    Finished example

--- public/main.js
+++ public/main.js
@@ -1,1 +1,26 @@
-console.log('Hello!');
+var OwnersDisplay = (function(){
+	var owners = [];
+	return {
+		oninit: function(){
+			m.request({
+				method: 'GET',
+				url: './owners'
+			}).then(function(response){
+				owners = response.body;
+			});
+		},
+		view: function(){
+			return [
+				m('ul', [
+					owners.map(function(owner){
+						return m('li', owner.email);
+					})
+				])
+			]
+		}
+	}
+})();
+
+document.addEventListener('DOMContentLoaded', function(){
+	m.mount(document.getElementById('display'), OwnersDisplay);
+});
